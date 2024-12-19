4dff22d069522e26e59214fc605cf5bc0da3928c
sacazidal <tw1nkle001@yandex.ru>
2024-12-19 22:59:30 +0700

    Адаптировал страницу

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="bg-neutral-900">
-      <div className="flex flex-col space-y-3 min-h-screen items-center justify-center relative">
-        <h1 className="z-10 absolute text-neutral-800 font-bold 2xl:text-uxl xl:text-mxl lg:text-lxl md:text-axl sm:text-gxl">
+      <div className="flex flex-col space-y-3 min-h-screen items-center justify-center relative p-5 md:p-0">
+        <h1 className="z-10 absolute text-neutral-800 font-bold text-gxl 2xl:text-uxl xl:text-mxl lg:text-lxl md:text-axl sm:text-gxl">
           404
         </h1>
-        <h2 className="font-bold text-6xl z-20 text-center">
+        <h2 className="font-bold text-3xl md:text-6xl z-20 text-center">
           Page not found
         </h2>
-        <p className="text-neutral-400 text-xl pb-4 z-20 text-center">
+        <p className="text-neutral-400 text-base md:text-xl pb-4 z-20 text-center">
           The page was not found, maybe you made a mistake?{" "}
         </p>
         <button
           type="button"
-          className="py-3 px-5 bg-red-700 hover:bg-rose-700 rounded-2xl z-20 flex justify-center items-center"
+          className="py-2 md:py-3 px-3 md:px-5 bg-red-700 hover:bg-rose-700 rounded-2xl z-20 flex justify-center items-center text-sm lg:text-base"
         >
           <Link to="/">Back to Homepage</Link>
         </button>
